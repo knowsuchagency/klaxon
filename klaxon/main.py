4e7e2fdbab984563780c93e4812e6186d5b6a20e
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2019-02-19 09:47:48 -0800

    blacken

--- klaxon/main.py
+++ klaxon/main.py
@@ -7,19 +7,25 @@ import shlex
 import sys
 
 
-def klaxon(message: str= '', title="Klaxon", subtitle="", sound=""):
+def klaxon(message: str = "", title="Klaxon", subtitle="", sound=""):
     """
     Wraps osascript.
 
     see https://apple.stackexchange.com/questions/57412/how-can-i-trigger-a-notification-center-notification-from-an-applescript-or-shel/115373#115373
     """
-    if not sys.platform == 'darwin':
-        logging.error('klaxon only works on Mac OS')
+    if not sys.platform == "darwin":
+        logging.error("klaxon only works on Mac OS")
         return
 
-    message, title, subtitle, sound = map(shlex.quote, (message, title, subtitle, sound))
-    command = f"""osascript -e 'display notification "{message}" with title "{title}" subtitle "{subtitle}" sound name "{sound}"'"""
-    sp.run(shlex.split(command))
+    message, title, subtitle, sound = map(
+        shlex.quote, (message, title, subtitle, sound)
+    )
+
+    sp.run(
+        shlex.split(
+            f"""osascript -e 'display notification "{message}" with title "{title}" subtitle "{subtitle}" sound name "{sound}"'"""
+        )
+    )
 
 
 def klaxonify(
